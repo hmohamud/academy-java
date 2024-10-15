99411fb05c76623a592e978a3ab20135fce17ff6
Hamza Mohamud <101277252+hmohamud@users.noreply.github.com>
2024-10-15 15:36:40 -0400

    add exercise files week4

---
+++ src/main/java/com/bptn/course/_20_tree_set/Main.java
@@ -0,0 +1,14 @@
+package com.bptn.course._20_tree_set;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i);
+			if (i % 2 == 0) {
+				System.out.print(" - even");
+			}
+			System.out.println();
+		}
+		System.out.print("We love to print lines.");
+	}
+}
---
+++ src/main/java/com/bptn/course/_20_tree_set/MyClass.java
@@ -0,0 +1,9 @@
+package com.bptn.course._20_tree_set;
+
+public class MyClass {
+	private int privateVariable = 10;
+
+	private void printPrivateVariable() {
+		System.out.println("Private variable: " + privateVariable);
+	}
+}
